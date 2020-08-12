ac3055d313307e78e15273de9c15cffcb748079a
heqro <heqromancer@gmail.com>
2020-08-12 17:18:07 +0200

    Install script now echoes messages explaining what's going on

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-# STEP ONE: Copying archives
-# 1 - Copying over wallpapers (user-wide)
+    echo "1 - Copying over wallpapers (user-wide)..."
     mkdir ~/.local/share/wallpapers
     \cp -r wallpapers ~/.local/share/wallpapers
-# 2 - Copying over fonts (system-wide)
+    echo "2 - Copying over fonts (system-wide)..."
     sudo \cp -r fonts/fira-sans /usr/share/fonts
     sudo \cp -r fonts/fira-sans-code /usr/share/fonts
-# 3 - Copying over color schemes, GTK themes and aurorae
+    echo "3 - Copying over color schemes, GTK themes and aurorae"
     \cp -r themes/color-profiles ~/.local/share/color-schemes
     \cp -r themes/gtk-whitesur-themes ~/.themes
     mkdir ~/.local/share/aurorae
     mkdir ~/.local/share/aurorae/themes
     \cp -r themes/aurorae ~/.local/share/aurorae/themes
 
-# 4 - Copying .desktop files as well as icons
+    echo "4 - Copying .desktop files as well as icons"
     sudo cp icons/heqro-sun.svg /usr/share/pixmaps
     sudo cp icons/heqro-night.svg /usr/share/pixmaps
     sudo cp icons/heqro-headset.png /usr/share/pixmaps
@@ -23,17 +22,17 @@
     \cp -r activity-changing/executables-with-script ~/.local/share/heqro-kde-scripting
     chmod -R +x ~/.local/share/heqro-kde-scripting/executables-with-script
     
-# 5 - Copying icons pack
+    echo "5 - Copying icons pack"
     mkdir ~/.local/share/icons
     
     tar -xf themes/icon-pack/03-Tela-circle-blue.tar.xz -C ~/.local/share/icons
 
-# 6 - Copying over cursor theme
+    echo "6 - Copying over cursor theme"
     mkdir ~/.icons
     
     tar -xf themes/cursor-themes/volantes_light_cursors.tar.gz -C ~/.icons
 
-# 7 - Activities set-up
+    echo "7 - Activities set-up"
 
     qdbus org.kde.ActivityManager /ActivityManager/Activities AddActivity "A1-Chill" 
     
@@ -45,12 +44,12 @@
     
     qdbus org.kde.ActivityManager /ActivityManager/Activities AddActivity "A5-Gaming" 
     
-# 8 - Marking scripts for controlling day and night themes executable
-    # - Marking theme script files as executable
+    echo "8 - Marking scripts for controlling day and night themes executable and moving files where it's due"
+    
     chmod +x theme-changing-scripts/ChangeTheme.sh
     chmod +x theme-changing-scripts/ChangeToDayTheme.desktop
     chmod +x theme-changing-scripts/ChangeToNightTheme.desktop
-    # - Moving files where it's due
+    
     \cp -r theme-changing-scripts ~/.local/share/heqro-kde-scripting
     
     
